933ae247a3013cb21fbd15cde581f781889b775d
herlufba <herluf@safeex.com>
2024-12-31 10:34:04 +0100

    use simpler squares

--- src/board.rs
+++ src/board.rs
@@ -60,69 +60,10 @@ impl Display for Piece {
     }
 }
 
-/// Every square in chess.
-#[rustfmt::skip]
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
-pub enum Square {
-    A1, B1, C1, D1, E1, F1, G1, H1,
-    A2, B2, C2, D2, E2, F2, G2, H2,
-    A3, B3, C3, D3, E3, F3, G3, H3,
-    A4, B4, C4, D4, E4, F4, G4, H4,
-    A5, B5, C5, D5, E5, F5, G5, H5,
-    A6, B6, C6, D6, E6, F6, G6, H6,
-    A7, B7, C7, D7, E7, F7, G7, H7,
-    A8, B8, C8, D8, E8, F8, G8, H8,
-}
-
-impl Square {
-    pub fn rank(&self) -> u8 {
-        use Square::*;
-        match self {
-            A1 | B1 | C1 | D1 | E1 | F1 | G1 | H1 => 0,
-            A2 | B2 | C2 | D2 | E2 | F2 | G2 | H2 => 1,
-            A3 | B3 | C3 | D3 | E3 | F3 | G3 | H3 => 2,
-            A4 | B4 | C4 | D4 | E4 | F4 | G4 | H4 => 3,
-            A5 | B5 | C5 | D5 | E5 | F5 | G5 | H5 => 4,
-            A6 | B6 | C6 | D6 | E6 | F6 | G6 | H6 => 5,
-            A7 | B7 | C7 | D7 | E7 | F7 | G7 | H7 => 6,
-            A8 | B8 | C8 | D8 | E8 | F8 | G8 | H8 => 7,
-        }
-    }
-
-    pub fn file(&self) -> u8 {
-        use Square::*;
-        match self {
-            A1 | A2 | A3 | A4 | A5 | A6 | A7 | A8 => 0,
-            B1 | B2 | B3 | B4 | B5 | B6 | B7 | B8 => 1,
-            C1 | C2 | C3 | C4 | C5 | C6 | C7 | C8 => 2,
-            D1 | D2 | D3 | D4 | D5 | D6 | D7 | D8 => 3,
-            E1 | E2 | E3 | E4 | E5 | E6 | E7 | E8 => 4,
-            F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 => 5,
-            G1 | G2 | G3 | G4 | G5 | G6 | G7 | G8 => 6,
-            H1 | H2 | H3 | H4 | H5 | H6 | H7 | H8 => 7,
-        }
-    }
-
-    pub fn from_rank_and_file(rank: usize, file: usize) -> Option<Self> {
-        SQUARES.get(rank * 8 + file).copied()
-    }
-
-    /// Gets a u64 with a 1 bit at the position corresponding to this square.
-    pub fn bit_mask(&self) -> u64 {
-        1u64 << self.rank() * 8 + self.file()
-    }
-}
-
-/// Every square, for convenient iteration!
-/// Note that the order matters here. Piece::from_rank_and_file relies on it.
-pub const SQUARES: [Square; 64] = {
-    use Square::*;
-    [
-        A1, B1, C1, D1, E1, F1, G1, H1, A2, B2, C2, D2, E2, F2, G2, H2, A3, B3, C3, D3, E3, F3, G3,
-        H3, A4, B4, C4, D4, E4, F4, G4, H4, A5, B5, C5, D5, E5, F5, G5, H5, A6, B6, C6, D6, E6, F6,
-        G6, H6, A7, B7, C7, D7, E7, F7, G7, H7, A8, B8, C8, D8, E8, F8, G8, H8,
-    ]
-};
+/// An integer representing a square on a chess board.
+/// There are 64 unique positions on the board, so we actually only need 6 bits.
+/// A u8 suffices for now.
+pub type Square = u8;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub struct Move {
@@ -201,17 +142,17 @@ impl Board {
         let mut board = Self::blank();
         let placement = fields[0];
         for (rank_idx, rank_str) in placement.split('/').enumerate() {
-            let rank = 7 - rank_idx;
-            let mut file = 0;
+            let rank: u8 = 7 - (rank_idx as u8);
+            let mut file: u8 = 0;
             for piece in rank_str.chars() {
                 match piece {
                     '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' => {
                         // Skip this amount of squares
-                        file += piece.to_string().parse::<usize>().unwrap();
+                        file += piece.to_string().parse::<u8>().unwrap();
                     }
                     p => {
                         if let Some(valid) = Piece::from_char(&p) {
-                            board.place(valid, Square::from_rank_and_file(rank, file).unwrap());
+                            board.place(valid, rank * 8 + file);
                             file += 1
                         } else {
                             panic!("Not a valid FEN string: '{fen}'")
@@ -221,11 +162,20 @@ impl Board {
             }
         }
 
+        // Read whose turn it is.
+        match fields[1] {
+            "w" => board.white_to_move = true,
+            "b" => board.white_to_move = false,
+            _ => panic!("Not a valid FEN string: '{fen}'"),
+        }
+
+        // TODO: Read the rest of the FEN string.
+
         board
     }
 
     fn clear(&mut self, square: Square) {
-        let mask = !square.bit_mask();
+        let mask = !(1u64 << square);
         self.pawns_black &= mask;
         self.bishops_black &= mask;
         self.knights_black &= mask;
@@ -243,7 +193,7 @@ impl Board {
     fn place(&mut self, piece: Piece, at: Square) {
         self.clear(at); // Clear any pieces off the target square.
 
-        let mask = at.bit_mask();
+        let mask = 1u64 << at;
         match piece {
             Piece::PawnWhite => self.pawns_white |= mask,
             Piece::KnightWhite => self.knights_white |= mask,
@@ -272,7 +222,7 @@ impl Board {
 
     /// Lookup what piece is at a particular square in the board.
     pub fn at(&self, square: Square) -> Option<Piece> {
-        let mask = square.bit_mask();
+        let mask = 1u64 << square;
         if self.pawns_white & mask >= 1 {
             Some(Piece::PawnWhite)
         } else if self.pawns_black & mask >= 1 {
@@ -301,6 +251,27 @@ impl Board {
             None
         }
     }
+
+    pub fn generate_moves(&self) -> Vec<Move> {
+        let moves = Vec::new();
+
+        for r in (0..8).rev() {
+            for f in 0..8 {
+                let square = r * 8 + f;
+                match self.at(square) {
+                    None => continue, // Square was empty
+                    Some(p) => {
+                        use Piece::*;
+                        match p {
+                            _ => todo!(),
+                        }
+                    }
+                }
+            }
+        }
+
+        moves
+    }
 }
 
 impl Display for Board {
@@ -311,7 +282,7 @@ impl Display for Board {
         for r in (0..8).rev() {
             out.push_str(format!("{} | ", r + 1).as_str());
             for f in 0..8 {
-                let square = Square::from_rank_and_file(r, f).unwrap();
+                let square = r * 8 + f;
                 match self.at(square) {
                     None => out.push_str("  "),
                     Some(piece) => out.push_str(format!("{} ", piece).as_str()),
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use board::Board;
 
 fn main() {
     let standard_with_e4 = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1";
-    let crazy = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -";
-    let board = Board::from_fen_string(crazy);
-    println!("{}", board);
+    let board = Board::from_fen_string(standard_with_e4);
+    board.generate_moves();
 }
